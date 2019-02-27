aa3f7419696c981734d4e388d502fb7a660b47a9
Chris <challer6@gmail.com>
2019-02-26 18:17:11 -0800

    Implement JSON parsing in JSON utils and populateUI method in detailed activity class

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,12 +3,16 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -61,22 +65,48 @@ public class DetailActivity extends AppCompatActivity {
      * TODO
      */
     private void populateUI(Sandwich sandwich) {
-        // Check to make sure that the sandwich exists, and return null if it doens't
+        // Check to make sure that the sandwich exists, and log the error if it doens't
+        if (sandwich == null) {
+            Log.e("DetailActivity: ", "The input sandwich object was null");
+        }
 
         /* Set the text of the alsoKnownAs TextView. If there isn't any information,
            set the TextView to say "This information isn't available" */
+        TextView alsoKnownAsTV = (TextView) findViewById(R.id.also_known_tv);
+
+        /* For each item in the array of alsoKnownAs names, append it to a string
+           separated by commas */
+        List<String> altNamesList = sandwich.getAlsoKnownAs();
+        String altNames = "";
+        for (String item : altNamesList) {
+            altNames += item + " | ";
+        }
+        alsoKnownAsTV.setText(altNames);
+
 
         /* Set the text of the ingredients TextView. If there isn't any information,
            set the TextView to say "This information isn't available" */
-            /* For each item in the array of ingredients, append it to a string
+        TextView ingredientsTV = (TextView) findViewById(R.id.ingredients_tv);
+
+        /* For each item in the array of ingredients, append it to a string
                separated by commas */
+        List<String> ingredientsList = sandwich.getIngredients();
+        String ingredientsString = "";
+        for (String item : ingredientsList) {
+            ingredientsString += item + " | ";
+        }
+        ingredientsTV.setText(ingredientsString);
+
 
         /* Set the text of the description TextView. If there isn't any information,
            set the TextView to say "This information isn't available" */
+        TextView descriptionTV = (TextView) findViewById(R.id.description_tv);
+        descriptionTV.setText(sandwich.getDescription());
+
 
         /* Set the text of the place of origin TextView. If there isn't any information,
            set the TextView to say "This information isn't available" */
-
-
+        TextView placeOfOriginTV = (TextView) findViewById(R.id.origin_tv);
+        placeOfOriginTV.setText(sandwich.getPlaceOfOrigin());
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,42 +1,83 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
     /**
      * TODO
      *
-     * @param json
-     * @return
+     * @param json  The string form of the JSON that needs to be parsed.
+     * @return  A Sandwich object created from the JSON string, with the revelent data members filled in.
      */
     public static Sandwich parseSandwichJson(String json) {
+        // Check to see if the json string is empty; return null and log it if it is.
+        if (json.isEmpty()) {
+            Log.e("JsonUtils", "The input JSON string was empty");
+            return null;
+        }
 
-        // Try {
+        // Initialize JSON object and extract the relevent data
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+
             // Extract name object
+            JSONObject name = sandwichJSON.getJSONObject("name");
 
-                // Extract mainName value
+            // Extract mainName value
+            String mainName = name.getString("mainName");
 
-                // Extract alsoKnownAs array
+            // Extract alsoKnownAs array
+            JSONArray alsoKnownAsArray = name.getJSONArray("alsoKnownAs");
+            List<String> alsoKnownAs = new ArrayList<>();
+            for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                alsoKnownAs.add(alsoKnownAsArray.getString(i));
+            }
 
             // Extract placeOfOrigin value
+            String placeOfOrigin = sandwichJSON.getString("placeOfOrigin");
 
             // Extract description value
+            String description = sandwichJSON.getString("description");
 
             // Extract image string value
+            String imageLocation = sandwichJSON.getString("image");
 
-            // Extract ingerdients array
+            // Extract ingredients array
+            JSONArray ingredientsArray = sandwichJSON.getJSONArray("ingredients");
 
-                // For each item in JSON array, store the value in a string array
-
+            // For each item in JSON array, store the value in a string array
+            List<String> ingredients = new ArrayList<>();
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                ingredients.add(ingredientsArray.getString(i));
+            }
 
             // Create a new sandwich object and fill it with the data extracted above
+            Sandwich newSandwich = new Sandwich();
+            newSandwich.setMainName(mainName);
+            newSandwich.setAlsoKnownAs(alsoKnownAs);
+            newSandwich.setDescription(description);
+            newSandwich.setPlaceOfOrigin(placeOfOrigin);
+            newSandwich.setIngredients(ingredients);
+            newSandwich.setImage(imageLocation);
+
+            return newSandwich;
 
 
-
-
-        // Catch JSON error
-            // If an error has ocurred
+        }
+        // Catch JSONException error
+        catch (JSONException e) {
+            Log.e("JsonUtils", e.toString());
             return null;
+        }
     }
 }
